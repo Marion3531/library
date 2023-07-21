9d85bbd908eeb597505ff465fd1f53f65253caab
unknown <Marion3531>
2023-07-21 13:53:16 +0100

    check if book already borrowed

--- src/main/java/com/example/library/service/LoanService.java
+++ src/main/java/com/example/library/service/LoanService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.library.exception.BookAlreadyBorrowedException;
 import com.example.library.model.Book;
 import com.example.library.model.Loan;
 import com.example.library.repository.LoanRepository;
@@ -33,14 +34,19 @@ public class LoanService {
 		
 		Book book = bookService.getBookById(bookId);
 		
-		Loan newLoan = new Loan();
-		newLoan.setBook(book);
-		newLoan.setBorrowedBy(loan.getBorrowedBy());
-		newLoan.setBorrowingDate(new Date());
-		newLoan.setBorrowingPeriod(15);
-		newLoan.setBorrowed(true);
-		
-		return repository.save(newLoan);
+		if() {
+			Loan newLoan = new Loan();
+			newLoan.setBook(book);
+			newLoan.setBorrowedBy(loan.getBorrowedBy());
+			newLoan.setBorrowingDate(new Date());
+			newLoan.setBorrowingPeriod(15);
+			newLoan.setBorrowed(true);
+			
+			return repository.save(newLoan);
+		}else{
+			throw new BookAlreadyBorrowedException(bookId);
+		}
+	
 	}
 	
 	//DELETE -> delete loan/return the book
